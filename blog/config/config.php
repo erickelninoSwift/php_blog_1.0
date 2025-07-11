a19fe33697c0865146357e5c5e9d8b9018ee1f77
Erick El nino <erick.tshimbombo@yahoo.com>
2025-07-12 00:44:31 +0200

    established database connection

---
+++ blog/config/config.php
@@ -0,0 +1,24 @@
+<?php 
+    //host
+    $host = 'localhost';
+    //bd name
+
+    $db_name = 'blog';
+
+    // username
+    $user_name = 'root';
+
+    //password
+    $password = 'root';
+     
+
+    // PDO connection
+    $connection = new PDO("mysql:host=$host;dbname=$db_name", $user_name, $password);
+
+    if ($connection === true) {
+        echo "connection successful";
+    } else {
+        echo "connection error";
+    };
+
+?>;
