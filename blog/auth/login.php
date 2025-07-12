167acf3c9f0b8048083550c4da39501ca94184ac
Erick El nino <erick.tshimbombo@yahoo.com>
2025-07-12 14:34:00 +0200

    done with auth and now starting with sessions in our application

--- blog/auth/login.php
+++ blog/auth/login.php
@@ -1,5 +1,50 @@
 <?php require "../includes/header.php" ?>
+<!-- connection -->
+<?php require "../config/config.php"  ?>
+<?php
+    if($_SERVER['REQUEST_METHOD'] === "POST") {
+        //
+    // check if the submit 
+    if(isset($_POST["submit"])) {
+        //
+        if (empty($_POST["email"])|| empty($_POST['password']) ) {
+            
+         echo "error : provide all fields";
+       
+       } else {
+        // save all fields in variables
 
+         $email = trim($_POST["email"]);
+         $password = trim($_POST["password"]);
+         // write query
+
+         $query = $connection->query("SELECT * FROM users WHERE email='$email' LIMIT 1");
+         // execute and fetch
+          $query->execute();
+         // do our row count fetch as an aosociative array
+         $row = $query->fetch(PDO::FETCH_ASSOC);
+         // do our passport verify + redirect to the index page
+         $row_cout = $query->rowCount();
+         if($row_cout > 0) {
+            // if user was found then everything will happen here 
+            $user_name = $row["user_name"];
+        
+            if (password_verify($password, $row["password"])) {
+               //
+               echo "user have logged in";
+               header("location: ../index.php");
+            }else {
+                echo "wrong password was provided";
+            }
+            
+         }else {
+
+            echo "User was not found";
+         }
+       }
+
+    }}
+?>
 <form method="POST" action="login.php">
     <!-- Email input -->
     <div class="form-outline mb-4">
@@ -20,8 +65,6 @@
     <!-- Register buttons -->
     <div class="text-center">
         <p>a new member? Create an acount<a href="register.php"> Register</a></p>
-
-
 
     </div>
 </form>
