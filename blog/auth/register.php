be2ea77ab306a4eccfc43d3c7225c2f9b73748f9
Erick El nino <erick.tshimbombo@yahoo.com>
2025-07-12 02:27:12 +0200

    completed the registration process with pasword hasing

--- blog/auth/register.php
+++ blog/auth/register.php
@@ -1,146 +1,72 @@
+<!-- header -->
+<?php require "../includes/header.php"; ?>
+<?php require "../config/config.php"  ?>
+
 <?php
-    //
-?>
 
-<!DOCTYPE html>
-<html lang="en">
+// check if the server metho is get or post
+      if($_SERVER['REQUEST_METHOD'] === "POST") {
+        //
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Clean Blog - Start Bootstrap Theme</title>
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-    <!-- Font Awesome icons (free version)-->
-    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-    <!-- Google fonts-->
-    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet"
-        type="text/css" />
-    <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800"
-        rel="stylesheet" type="text/css" />
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="../css/styles.css" rel="stylesheet" />
-</head>
+        if(isset($_POST["submit"])) {
+            //
+            if(empty($_POST["email"]) || empty($_POST['username']) || empty($_POST['password'])) {
+                echo 'Please make sure all fields are provided';
+            }else {
+                //continue with registration
+                $email = trim($_POST['email']);
+                $username = trim($_POST['username']);
+                $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                //query
+                $query = $connection->prepare("INSERT INTO users (email,user_name,password) VALUES (:email,:username,:password)");
+                //execute function thats hwere you bind your data 
+                $query ->execute([
+                    "email"=> $email,
+                    "username"=> $username,
+                    "password"=> $password
+                ]);
 
-<body>
-    <!-- Navigation-->
-    <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
-        <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="index.html">Start Bootstrap</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                Menu
-                <i class="fas fa-bars"></i>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ms-auto py-4 py-lg-0">
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="../index.html">Home</a></li>
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="../posts/create.html">create</a>
-                    </li>
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="login.html">login</a></li>
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="register.html">register</a></li>
+                //redirect us to the login page
 
+                header("location: login.php");
+                
+            };
+        }
 
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="../contact.html">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!-- Page Header-->
-    <header class="masthead" style="background-image: url('../assets/img/home-bg.jpg')">
-        <div class="container position-relative px-4 px-lg-5">
-            <div class="row gx-4 gx-lg-5 justify-content-center">
-                <div class="col-md-10 col-lg-8 col-xl-7">
-                    <div class="site-heading">
-                        <h1>Clean Blog</h1>
-                        <span class="subheading">A Blog Theme by Start Bootstrap</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
+      }
+ ?>
 
-    <!-- Main Content-->
-    <div class="container px-4 px-lg-5">
+<form method="POST" action="register.php">
+    <!-- Email input -->
+    <div class="form-outline mb-4">
+        <input type="email" name="email" id="form2Example1" class="form-control" placeholder="Email" />
 
-        <form method="POST" action="register.php">
-            <!-- Email input -->
-            <div class="form-outline mb-4">
-                <input type="email" name="email" id="form2Example1" class="form-control" placeholder="Email" />
+    </div>
 
-            </div>
+    <div class="form-outline mb-4">
+        <input type="" name="username" id="form2Example1" class="form-control" placeholder="Username" />
 
-            <div class="form-outline mb-4">
-                <input type="" name="username" id="form2Example1" class="form-control" placeholder="Username" />
+    </div>
 
-            </div>
+    <!-- Password input -->
+    <div class="form-outline mb-4">
+        <input type="password" name="password" id="form2Example2" placeholder="Password" class="form-control" />
 
-            <!-- Password input -->
-            <div class="form-outline mb-4">
-                <input type="password" name="password" id="form2Example2" placeholder="Password" class="form-control" />
-
-            </div>
+    </div>
 
 
 
-            <!-- Submit button -->
-            <button type="submit" name="submit" class="btn btn-primary  mb-4 text-center">Register</button>
+    <!-- Submit button -->
+    <button type="submit" name="submit" class="btn btn-primary  mb-4 text-center">Register</button>
 
-            <!-- Register buttons -->
-            <div class="text-center">
-                <p>Aleardy a member? <a href="login.php">Login</a></p>
-
-
-
-            </div>
-        </form>
+    <!-- Register buttons -->
+    <div class="text-center">
+        <p>Aleardy a member? <a href="login.php">Login</a></p>
 
 
 
     </div>
-    <!-- Footer-->
-    <footer class="border-top">
-        <div class="container px-4 px-lg-5">
-            <div class="row gx-4 gx-lg-5 justify-content-center">
-                <div class="col-md-10 col-lg-8 col-xl-7">
-                    <ul class="list-inline text-center">
-                        <li class="list-inline-item">
-                            <a href="#!">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fas fa-circle fa-stack-2x"></i>
-                                    <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a href="#!">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fas fa-circle fa-stack-2x"></i>
-                                    <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a href="#!">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fas fa-circle fa-stack-2x"></i>
-                                    <i class="fab fa-github fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                    </ul>
-                    <div class="small text-center text-muted fst-italic">Copyright &copy; Your Website 2022</div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Core theme JS-->
-    <script src="../js/script.js"></script>
-</body>
+</form>
 
-</html>
+<!-- Footer-->
+<?php require "../includes/footer.php"?>
