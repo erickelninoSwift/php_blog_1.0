777dc8ec82a08efcb5191ac0dd7453e863065e72
Erick El nino <erick.tshimbombo@yahoo.com>
2025-07-12 17:34:44 +0200

    fixed all naviagtion issues

--- blog/includes/header.php
+++ blog/includes/header.php
@@ -38,8 +38,10 @@
                     <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="../index.php">Home</a></li>
                     <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="../posts/create.php">create</a>
                     </li>
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="login.php">login</a></li>
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="register.php">register</a></li>
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="../auth/login.php">login</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4"
+                            href="../auth/register.php">register</a></li>
 
 
                     <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="../contact.php">Contact</a>
--- blog/includes/home_header.php
+++ blog/includes/home_header.php
@@ -35,14 +35,16 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto py-4 py-lg-0">
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="../index.php">Home</a></li>
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="../posts/create.php">create</a>
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="./index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="./posts/create.php">create</a>
                     </li>
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="login.php">login</a></li>
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="register.php">register</a></li>
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="./auth/login.php">login</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4"
+                            href="./auth/register.php">register</a></li>
 
 
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="../contact.php">Contact</a>
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="./contact.php">Contact</a>
                     </li>
                 </ul>
             </div>
