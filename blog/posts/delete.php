66a1fe4b80aa4059e2bb0ca8fdc4c86857b389cd
Erick El nino <erick.tshimbombo@yahoo.com>
2025-07-19 02:06:10 +0200

    managed to delete a post

--- blog/posts/delete.php
+++ blog/posts/delete.php
@@ -1,16 +1,19 @@
-<?php include "../config/config.php" ?>
+<?php include "../config/config.php"; ?>
 <?php
-     
-
+    
 if(isset($_GET['del_id'])) {
     //
     $post_id = $_GET['del_id'];
-    $delete_query = $connection->query("DELETE *  FROM posts WHERE id= :id");
-
-    $delete_query->execute([
-        "id" => $post_id
-    ]);
+    echo $post_id;
+    // $delete_query = $connection->query("DELETE *  FROM posts WHERE id= :id");
+     $delete_query = $connection->prepare("DELETE FROM posts WHERE id=:current_post_id");
+     
+     $delete_query->execute([
+        "current_post_id" => $post_id,
+     ]);
     
     header('location: http://localhost:8888/blog/blog_project_1.0/blog/index.php');
+
+    exit();
 }
 ?>
