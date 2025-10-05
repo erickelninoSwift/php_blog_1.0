5130956825db467da91ac1e0ea0d258483f0d365
Erick El nino <erick.tshimbombo@yahoo.com>
2025-10-05 20:58:01 +0200

    managed to add delete

--- blog/admin/categories/delete_categories.php
+++ blog/admin/categories/delete_categories.php
@@ -1,3 +1,30 @@
-<?php 
-   
-   echo "Delete Categories";
+<?php include __DIR__ . "/../../config/config.php"; ?>
+<?php define('BASE_URL', '/blog/blog_project_1.0/blog/');
+session_start();
+   function base_url($path) {
+         return BASE_URL . $path . '.php';
+   }
+   function base_styles() {
+            return BASE_URL . 'admin/styles/style.css';
+   }
+ 
+?>
+<?php  
+     if(isset($_GET['id'])) {
+          $category_id = $_GET['id'];
+          $query = "DELETE FROM categories WHERE id = :id";
+          $stmt = $connection->prepare($query);
+          $stmt->bindParam(':id', $category_id);
+          $stmt->execute();
+          if($stmt->rowCount() > 0) {
+               header("Location: " . base_url('admin/categories/show'));
+               exit();
+          } else {
+               echo "<div class='alert alert-danger' role='alert'>Error deleting category.</div>";
+          }
+     } else {
+          header("Location: " . base_url('admin/categories/show'));
+          exit();
+     }
+
+?>
