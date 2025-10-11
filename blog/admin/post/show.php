a2725492b00f6cedac7f7617821785e628273aa6
Erick El nino <erick.tshimbombo@yahoo.com>
2025-10-11 02:54:05 +0200

    done

--- blog/admin/post/show.php
+++ blog/admin/post/show.php
@@ -1,13 +1,26 @@
 <?php include __DIR__ . "/../layout/header.php";  ?>
 <?php 
-   $query = "SELECT posts.*, users.*, categories.*
-          FROM posts 
-          JOIN users ON posts.user_id = users.user_id 
-          JOIN categories ON posts.category_id = categories.id";
+   $query = "SELECT 
+    posts.id AS post_id,
+    posts.title,
+    posts.subtitle,
+    posts.body,
+    posts.img,
+    posts.created_at AS post_created_at,
+    users.user_id AS author_id,
+    users.user_name AS author_name,
+    users.email AS author_email,
+    categories.id AS category_id,
+    categories.name AS category_name
+FROM posts
+JOIN users ON posts.user_id = users.user_id
+JOIN categories ON posts.category_id = categories.id
+";
+
    $stmt = $connection->prepare($query);
    $stmt->execute();
    $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+  
    if($stmt->rowCount() == 0){
        echo "<h1 class='text-center text-danger'>No Posts Found</h1>";
        die();
@@ -29,6 +42,7 @@
                                 <th scope="col">#</th>
                                 <th scope="col">title</th>
                                 <th scope="col">category</th>
+                                <th scope="col">status</th>
                                 <th scope="col">user</th>
                                 <th scope="col">delete</th>
                             </tr>
@@ -38,10 +52,24 @@
                             <tr>
                                 <th scope="row"><?php echo $index + 1; ?></th>
                                 <td><?php echo $post['title']; ?></td>
-                                <td><?php echo $post['name']; ?></td>
-                                <td><?php echo $post['user_name']; ?></td>
-                                <td><a href="http://localhost:8888/blog/blog_project_1.0/blog/admin/post/delete_post.php?id=<?php echo $post['id']; ?>"
-                                        class="btn btn-danger  text-center ">delete</a>
+                                <td><?php echo $post['category_name']; ?></td>
+                                <td>
+                                    <span class="badge bg-success text-white"
+                                        style="padding: 8px 14px; font-size: 0.8rem;">
+                                        Active
+                                    </span>
+                                </td>
+                                <td><?php echo $post['author_name']; ?></td>
+                                <td>
+                                    <a href="http://localhost:8888/blog/blog_project_1.0/blog/admin/post/update_post.php?id=<?php echo $post['post_id']; ?>"
+                                        class="btn btn-warning btn-sm text-center me-2">
+                                        Update
+                                    </a>
+
+                                    <a href="http://localhost:8888/blog/blog_project_1.0/blog/admin/post/delete_post.php?id=<?php echo $post['post_id']; ?>"
+                                        class="btn btn-danger btn-sm text-center">
+                                        Delete
+                                    </a>
                                 </td>
                             </tr>
                             <?php endforeach; ?>
